bb0186f821610775cdd46c9fc3178ed601a69e86
KaishaerMahemuti <k.mahemuti@gmail.com>
2024-08-23 09:23:38 -0400

    updated the text

--- portfolio.js
+++ portfolio.js
@@ -1,12 +1,13 @@
 function toggleDropdown() {
-    const dropdownContent = document.getElementById("contactButton");
+    const dropdownContent = document.getElementById("contactInfo");
     dropdownContent.style.display = dropdownContent.style.display === "block" ? "none" : "block";
 }
 
 // close the dropdown when out side is clicked 
 window.onclick = function(event) {
-    const dropdownContent = document.getElementById("contactButton");
+    const dropdownContent = document.getElementById("contactInfo");
     if (!event.target.matches('.dropdown-btn') && !dropdownContent.contains(event.target)) {
         dropdownContent.style.display = "none";
     }
 };
+  ``
